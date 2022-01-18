7d7ddd244104bb7dc9075a5479962253e6c7eeae
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2022-01-18 21:03:56 +0100

    snack-1

--- index.php
+++ index.php
@@ -11,13 +11,73 @@ A domani :faccia_leggermente_sorridente: -->
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snack 1</title>
 </head>
+
 <body>
-  
+    <?php
+    $partite = [
+        [
+            'squadraCasa' => 'Trieste',
+            'squadraTrasferta' => 'Reggiana',
+            'puntiCasa' => 83,
+            'puntiTrasferta' => 85
+        ],
+        [
+            'squadraCasa' => 'Brescia',
+            'squadraTrasferta' => 'Brescia',
+            'puntiCasa' => 88,
+            'puntiTrasferta' => 67
+        ],
+        [
+            'squadraCasa' => 'Basket Napoli',
+            'squadraTrasferta' => 'Fortitudo Bologna',
+            'puntiCasa' => 86,
+            'puntiTrasferta' => 89
+        ],
+        [
+            'squadraCasa' => 'Pesaro',
+            'squadraTrasferta' => 'Olimpia Milano',
+            'puntiCasa' => 85,
+            'puntiTrasferta' => 82
+        ],
+        [
+            'squadraCasa' => 'Sassari',
+            'squadraTrasferta' => 'Trento',
+            'puntiCasa' => 88,
+            'puntiTrasferta' => 80
+        ],
+        [
+            'squadraCasa' => 'Varese',
+            'squadraTrasferta' => 'Venezia',
+            'puntiCasa' => 76,
+            'puntiTrasferta' => 68
+        ],
+        [
+            'squadraCasa' => 'Virtus Bologna',
+            'squadraTrasferta' => 'Treviso',
+            'puntiCasa' => 84,
+            'puntiTrasferta' => 66
+        ],
+        [
+            'squadraCasa' => 'Tortona',
+            'squadraTrasferta' => 'Cremona',
+            'puntiCasa' => 97,
+            'puntiTrasferta' => 92
+        ],
+    ]
+    ?>
+    <h3>Risultati</h3>
+    <ul>
+        <?php for ($i = 0; $i < count($partite); $i++) { ?>
+            <?php $match = $partite[$i]; ?>
+            <li><?php echo $match['squadraCasa'] . ' - ' . $match['squadraTrasferta'] . ' | ' . $match['puntiCasa'] . ' - ' . $match['puntiTrasferta']; ?></li>
+        <?php } ?>
+    </ul>
 </body>
+
 </html>
